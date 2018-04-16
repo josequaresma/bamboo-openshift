51f4860f3c842a6861f5bfe9b71bdbe1e28ef458
Jose Quaresma <jose.quaresma@accenture.com>
2018-04-16 19:29:12 +0200

    Adding first try at the bamboo agent

---
+++ bamboo-agent/dockerfile/run.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# Exit on errors
+set -e
+
+BAMBOO_INSTALLER=$BAMBOO_AGENT_INSTALL/$BAMBOO_AGENT_JAR
+if [ -f $BAMBOO_INSTALLER ]; then
+	echo "-> Installer already found at $BAMBOO_INSTALLER. Skipping download."
+else
+  # Download the agent from $DOWNLOAD_URL
+  DOWNLOAD_URL=http://$BAMBOO_SERVER:8085/agentServer/agentInstaller/$BAMBOO_AGENT_JAR
+  curl -SL $DOWNLOAD_URL -o $BAMBOO_AGENT_INSTALL/$BAMBOO_AGENT_JAR
+fi
+
+# Fix permissions
+#chown -R daemon:daemon $BAMBOO_INSTALLER
+chmod -R 777 $BAMBOO_INSTALLER
+
+BAMBOO_AGENT=$BAMBOO_AGENT_HOME/bin/bamboo-agent.sh
+if [ ! -f $BAMBOO_AGENT ]; then
+  # Run the agent installer
+  echo "-> Running Bamboo Installer ..."
+  java -jar $BAMBOO_AGENT_INSTALL/$BAMBOO_AGENT_JAR http://$BAMBOO_SERVER:8085/agentServer/
+fi
+
+# Fix permissions
+#chown -R daemon:daemon $BAMBOO_AGENT
+chmod -R 777 $BAMBOO_AGENT
+
+# Run the Bamboo agent
+# $BAMBOO_AGENT console
